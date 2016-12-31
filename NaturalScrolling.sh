697d98a3b2b37bb9db7d5e67017819baf901ab4e
Devin Hagerty <devin.hagerty95@gmail.com>
2016-12-31 12:22:55 -0500

    Removed troubleshooting comments

--- NaturalScrolling.sh
+++ NaturalScrolling.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 ID="$(xinput list | grep "Elan" | cut -d "=" -f 2 | cut -f 1)"
-echo "$ID"
+
 PROP="$(xinput list-props 13 | grep "Scrolling Distance" | head -n 1 | cut -d " " -f 4 | cut -d "(" -f 2 | cut -d ")" -f 1)"
-echo "$PROP"
+
 xinput set-prop "$ID" "$PROP" -75 -75
