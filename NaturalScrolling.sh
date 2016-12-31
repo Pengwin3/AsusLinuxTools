8dd2ee461179ff03880cf3cca6d19294634584fd
Pengwin3 <devin.hagerty95@gmail.com>
2016-12-31 17:47:12 -0500

    No change

--- NaturalScrolling.sh
+++ NaturalScrolling.sh
@@ -3,4 +3,4 @@ ID="$(xinput list | grep "Elan" | cut -d "=" -f 2 | cut -f 1)"
 
 PROP="$(xinput list-props $ID | grep "Scrolling Distance" | head -n 1 | cut -d " " -f 4 | cut -d "(" -f 2 | cut -d ")" -f 1)"
 
-xinput set-prop "$ID" "$PROP" -75 -75
+	xinput set-prop "$ID" "$PROP" -75 -75
--- PalmDetectionFix.sh
+++ PalmDetectionFix.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 ID="$(xinput list | grep "Elan" | cut -d '=' -f 2 | cut -f 1)"
-
+echo "$ID"
 xinput set-prop "$ID" 351 1
 xinput set-prop "$ID" 352 5 5
